b55d2481b36dc8bb212683462f9dbd3661e22c75
lukassos <lukassos@gmail.com>
2017-07-09 02:34:56 +0200

    changed PID 1 to exec supervisord

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-exec /etc/init.d/redis start
-exec /etc/init.d/rabbitmq-server start
-exec /etc/init.d/sensu-server start
-exec /etc/init.d/sensu-api start
-exec /etc/init.d/uchiwa start
-
+exec supervisord
 
 exec "$@"
